23829ba2451b79c76c8d4c9d57fefd72a4041d2c
Sarthak Shukla <sarthak2019fractal@gmail.com>
2022-01-15 12:14:49 +0530

    Initial Commit

---
+++ src/data/invoice.js
@@ -0,0 +1,45 @@
+const invoice = {
+  id: "5df3180a09ea16dc4b95f910",
+  invoice_no: "201906-28",
+  balance: "$2,283.74",
+  company: "College",
+  email: "sarthak@gmail.com",
+  phone: "9897752881",
+  address: "IET LUCKNOW",
+  trans_date: "2022-01-12",
+  due_date: "2022-01-12",
+  items: [
+    {
+      sno: 1,
+      desc: "Solar Panel",
+      qty: 5,
+      rate: 405.89,
+    },
+    {
+      sno: 2,
+      desc: "Battery",
+      qty: 5,
+      rate: 373.11,
+    },
+    {
+      sno: 3,
+      desc: "Solar Batteries",
+      qty: 5,
+      rate: 458.61,
+    },
+    {
+      sno: 4,
+      desc: "Connection Meters",
+      qty: 10,
+      rate: 725.24,
+    },
+    {
+      sno: 5,
+      desc: "Protective Devices",
+      qty: 4,
+      rate: 141.02,
+    },
+  ],
+};
+
+export default invoice;
